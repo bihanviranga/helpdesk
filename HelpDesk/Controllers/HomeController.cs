562dcb3383f3af00a71f4e2eda06528ffdf78eda
dhfernando <hdilshan.fernando11@gmail.com>
2020-05-17 16:03:19 +0530

    init project

---
+++ HelpDesk/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using HelpDesk.Models;
+
+namespace HelpDesk.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public JsonResult Index()
+        {
+            return Json("Lets Dev Help Desk");
+        }
+
+        
+    }
+}
---
+++ HelpDesk/Models/Employee/EmployeeModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelpDesk.Models.Employee
+{
+    public class EmployeeModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Dep { get; set; }
+    }
+}
