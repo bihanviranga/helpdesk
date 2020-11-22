5fc5c1de99df2b9bf3226c99ddf228202dad5f8c
dhfernando <hdilshan.fernando11@gmail.com>
2020-11-22 22:01:27 +0530

    dev password reset

---
+++ HelpDesk/Entities/DataTransferObjects/User/ResetPasswordDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelpDesk.Entities.DataTransferObjects.User
+{
+    public class ResetPasswordDto
+    {
+        public string CompanyId { get; set; }
+        public string UserName { get; set; }
+    }
+}
